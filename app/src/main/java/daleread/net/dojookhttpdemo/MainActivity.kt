b896fa12646bd62e20fa7d1d0736b8c0171d9d9d
dale43211 <dale43211@hotmail,com>
2023-04-26 16:07:21 -0400

    // display the remote image right away or after a brief delay     // depending on whether or not the URL is available in onCreate     // just trying to get used to the Activity lifecycle

--- app/src/main/java/daleread/net/dojookhttpdemo/MainActivity.kt
+++ app/src/main/java/daleread/net/dojookhttpdemo/MainActivity.kt
@@ -1,21 +1,27 @@
 package daleread.net.dojookhttpdemo
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.graphics.BitmapFactory
-import android.widget.ImageView
+import android.util.Log
 import daleread.net.dojookhttpdemo.databinding.ActivityMainBinding
-// import kotlinx.android.synthetic.main.activity_main.*
-
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
 import okhttp3.*
 import java.io.IOException
-import java.io.InputStream
+import kotlinx.coroutines.launch as kotlinxCoroutinesLaunch
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private val imageUrl = "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRnPjw6RNYmW2vSwUNbwOHd7LMXsXu152mSYjlfO4gv0cJe22AYM6eoga0IwL_Tudmx2JnPf3Z9BmZ0kJGYlB2Z05P8rQqjru4bcnsRjmBbM_2_dVLaU5CpAtM&usqp=CAc"
+    // display the remote image right away or after a brief delay
+    // depending on whether or not the URL is available in onCreate
+    // just trying to get used to the Activity lifecycle
+
+    //var imageUrl = "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRnPjw6RNYmW2vSwUNbwOHd7LMXsXu152mSYjlfO4gv0cJe22AYM6eoga0IwL_Tudmx2JnPf3Z9BmZ0kJGYlB2Z05P8rQqjru4bcnsRjmBbM_2_dVLaU5CpAtM&usqp=CAc"
+    var imageUrl = ""
     private val okClient by lazy {
         OkHttpClient()
     }
@@ -29,8 +35,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.imageView.setImageResource(R.drawable.maria02)
+        lifecycleScope.kotlinxCoroutinesLaunch {
+            if (imageUrl == "") { changeImageUrl()}
+        }
+       Log.d("Main", "Hello")
+    }
 
 
+    private suspend fun  changeImageUrl(){
+        delay(6000)
+        Log.d("Coroutine", "World")
+        imageUrl = "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRnPjw6RNYmW2vSwUNbwOHd7LMXsXu152mSYjlfO4gv0cJe22AYM6eoga0IwL_Tudmx2JnPf3Z9BmZ0kJGYlB2Z05P8rQqjru4bcnsRjmBbM_2_dVLaU5CpAtM&usqp=CAc"
         loadImage()
     }
 
@@ -44,7 +59,6 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call, response: Response){
                 val inputStream = response.body?.byteStream()
                 val bitmap = BitmapFactory.decodeStream(inputStream)
-                // lateinit var imageView: ImageView
                 this@MainActivity.runOnUiThread{
                     binding.imageView.setImageBitmap(bitmap)
                 }
